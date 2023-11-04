33200a0f516c03e7b8a3b3ee287f392a673cb5f5
MuhammadAriqohFirjatullah <149862480+MuhammadAriqohFirjatullah@users.noreply.github.com>
2023-11-04 14:19:16 +0700

    Add files via upload

---
+++ task.java/task1.java
@@ -0,0 +1,10 @@
+public class task1{
+    public static void main(String[] args) {
+        for (int i = 0; i<=100; i++){
+        String name = "Muhammad Ariqoh Firjatullah";
+        String NPM = "G1A023033";
+        System.out.println("Nama : " + name);
+        System.out.println("NPM : " + NPM);
+    }
+        }
+}
---
+++ task.java/task4.java
@@ -0,0 +1,12 @@
+public class task4 {
+    public static void main(String[] args) {
+        String[] TempatWisataTujuansaya = {
+            "Bali", "Bandung", "Jepang", "Korea Selatan"
+        };
+        for (int i=0 ; i<TempatWisataTujuansaya.length; i++){
+            System.out.println(TempatWisataTujuansaya[i]);
+        }
+    }
+}
+
+
